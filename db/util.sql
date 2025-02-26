6e6b24425ffcc9b14beedc7f67a1a6f741948849
ennead-nine <ennead.tbc@gmail.com>
2025-02-25 16:58:05 -0800

    .

--- db/util.sql
+++ db/util.sql
@@ -3,16 +3,20 @@ SELECT * FROM vsduel
 
 -- All Data
 SELECT vsduel_commander_data.points, commander.name, alliance.tag, vsduel_data.day_of_week 
-FROM vsduel_commander_data, alliance, commander, vsduel_data 
+FROM vsduel_commander_data, alliance, commander, vsduel_data, vsduel_week
 WHERE vsduel_commander_data.alliance_id=alliance.id 
 AND vsduel_commander_data.commander_id=commander.id 
 AND vsduel_commander_data.vsduel_data_id=vsduel_data.id
+AND vsduel_data.vsduel_week_id=vsduel_week.id
+AND vsduel_week.vsweek_number=2
 
 --Alliance Data
 SELECT vsduel_alliance_data.points, vsduel_alliance_data.vsduel_points, alliance.tag, vsduel_data.day_of_week 
-FROM vsduel_alliance_data, alliance, vsduel_data
+FROM vsduel_alliance_data, alliance, vsduel_data, vsduel_week
 WHERE vsduel_alliance_data.alliance_id=alliance.id
 AND vsduel_alliance_data.vsduel_data_id=vsduel_data.id
+AND (vsduel_data.vsduel_week_id=vsduel_week.id
+AND vsduel_week.vsweek_number=2)
 
 --Individual Totals
 SELECT commander.name, alliance.tag AS alliance, SUM(vsduel_commander_data.points) AS total
@@ -23,11 +27,13 @@ GROUP BY commander.name
 ORDER BY total DESC
 
 --Individual Total Top 10
-SELECT commander.name, alliance.tag AS alliance, SUM(vsduel_commander_data.points) AS total
-FROM commander, vsduel_commander_data, alliance
+SELECT commander.name, alliance.tag AS alliance, SUM(vsduel_commander_data.points) AS total, vsduel_week.vsweek_number
+FROM commander, vsduel_commander_data, alliance, vsduel_week,vsduel_data
 WHERE vsduel_commander_data.commander_id=commander.id
 AND vsduel_commander_data.alliance_id=alliance.id
-GROUP BY commander.name
+AND vsduel_data.vsduel_week_id=vsduel_week.id
+AND vsduel_week.vsweek_number=2
+GROUP BY commander.name, vsduel_week.vsweek_number
 ORDER BY total DESC
 LIMIT 10
 
@@ -41,14 +47,26 @@ ORDER BY total
 LIMIT 10
 
 --Individual {DOW} Top 10
-SELECT commander.name, alliance.tag AS alliance, SUM(vsduel_commander_data.points) AS total
-FROM commander, vsduel_commander_data, alliance, vsduel_data
+SELECT commander.name, alliance.tag AS alliance, vsduel_commander_data.points
+FROM commander, vsduel_commander_data, alliance, vsduel_data, vsduel_week
 WHERE vsduel_commander_data.commander_id=commander.id
 AND vsduel_commander_data.alliance_id=alliance.id
 AND vsduel_commander_data.vsduel_data_id=vsduel_data.id
-AND vsduel_data.day_of_week='Saturday'
+AND vsduel_data.day_of_week='Monday'
+AND vsduel_data.vsduel_week_id=vsduel_week.id
+AND vsduel_week.vsweek_number=2
 GROUP BY commander.name
-ORDER BY total DESC
+ORDER BY points
+
+SELECT commander.name, alliance.tag AS alliance, vsduel_commander_data.points
+FROM commander, vsduel_commander_data, alliance, vsduel_data, vsduel_week
+WHERE vsduel_commander_data.commander_id=commander.id
+AND vsduel_commander_data.alliance_id=alliance.id
+AND vsduel_commander_data.vsduel_data_id=vsduel_data.id
+AND vsduel_data.vsduel_week_id=vsduel_week.id
+AND vsduel_week.vsweek_number=2
+GROUP BY commander.name
+ORDER BY points
 LIMIT 10
 
 --Individual {DOW} Bottom 10
@@ -63,13 +81,16 @@ ORDER BY total
 LIMIT 10
 
 --Individual Under 7.2 Million
-SELECT commander.name, alliance.tag AS alliance, AVG(vsduel_commander_data.points) AS average
-FROM commander, vsduel_commander_data, alliance, vsduel_data
+SELECT commander.name, alliance.tag AS alliance,  AVG(vsduel_commander_data.points) AS average
+FROM commander, vsduel_commander_data, alliance, vsduel_data, vsduel_week
 WHERE vsduel_commander_data.commander_id=commander.id
 AND vsduel_commander_data.alliance_id=alliance.id
 AND vsduel_commander_data.vsduel_data_id=vsduel_data.id
+AND (vsduel_data.vsduel_week_id=vsduel_week.id
+AND vsduel_week.vsweek_number=2)
 GROUP BY commander.name
 ORDER BY average
+
 
 --Individual By {Alliance}/{Day}
 SELECT commander.name, vsduel_commander_data.points
@@ -79,4 +100,19 @@ AND vsduel_commander_data.commander_id=commander.id
 AND vsduel_commander_data.vsduel_data_id=vsduel_data.id
 AND alliance.tag='szb'
 AND vsduel_data.day_of_week='Monday'
+
+SELECT commander.name, alliance.tag, vsduel_commander_data.points, vsduel_week.vsweek_number, vsduel_data.day_of_week
+FROM commander, alliance, vsduel_commander_data, vsduel_week, vsduel_data
+WHERE vsduel_commander_data.commander_id=commander.id
+AND vsduel_commander_data.vsduel_data_id=vsduel_data_id
+AND vsduel_data.vsduel_week_id=vsduel_week.id
+AND vsduel_commander_data.alliance_id=alliance.id
+AND alliance.tag="P4K"
+
+SELECT commander.name
+FROM commander, commander_data, alliance
+WHERE commander_data.commander_id=commander.id
+AND commander_data.alliance_id=alliance.id
+AND alliance.tag="P4K"
+ORDER BY commander.name
 
